e0104e76781f2d90d32a7a63cd78475fec7af933
HisRoyalRedness <keith@fletcher.org>
2018-04-20 12:50:43 +1200

    Partial IntegerToken rework to get bitwidth and sign working

--- HackerCalc.Common/Tokens/Literal tokens/IntegerToken.cs
+++ HackerCalc.Common/Tokens/Literal tokens/IntegerToken.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Globalization;
 using System.Numerics;
 using System.Text;
+using System.Linq;
+using System.Diagnostics;
 
 namespace HisRoyalRedness.com
 {
@@ -35,11 +37,24 @@ namespace HisRoyalRedness.com
         {
             IsSigned = isSigned;
             BitWidth = bitWidth;
+
+            if (!IsUnbound)
+                NormaliseInternal();
         }
 
         public IntegerToken(BigInteger typedValue, bool isSigned = true, IntegerBitWidth bitWidth = IntegerBitWidth.Unbound)
             : this(typedValue.ToString(), typedValue, isSigned, bitWidth)
         { }
+
+        static IntegerToken()
+        {
+            var bitWidths = Enum.GetValues(typeof(IntegerBitWidth)).Cast<IntegerBitWidth>().Where(bw => bw != IntegerBitWidth.Unbound);
+            foreach (var bitWidth in bitWidths)
+            {
+                _minAndMax.Add(new SignAndBitwidthPair(false, bitWidth), new MinAndMax(bitWidth, false));
+                _minAndMax.Add(new SignAndBitwidthPair(true, bitWidth), new MinAndMax(bitWidth, true));
+            }
+        }
 
         #region Parsing
         public static IntegerToken Parse(string value, bool isHex)
@@ -118,6 +133,28 @@ namespace HisRoyalRedness.com
         }
         #endregion Casting
 
+        void NormaliseInternal()
+        {
+            if (IsUnbound)
+                return;
+
+            var key = new SignAndBitwidthPair(IsSigned, BitWidth);
+            if (!_minAndMax.ContainsKey(key))
+                throw new ArgumentOutOfRangeException($"Could not find a minimum and maximum value for {(IsSigned ? "a signed" : "an unsigned")} {nameof(IntegerToken)} with a bit width of {(int)BitWidth}");
+
+            if (TypedValue > _minAndMax[key].Max || TypedValue < _minAndMax[key].Min)
+                TypedValue = TypedValue & _minAndMax[key].Mask;
+
+            if (TypedValue < 0)
+            {
+                if (IsSigned)
+                    TypedValue = _minAndMax[key].Mask + 1 + TypedValue;
+                else
+                    throw new ArgumentOutOfRangeException("Value cannot be less than zero if the integer is unsigned.");
+            }
+        }
+
+        [DebuggerDisplay("{DisplayString}")]
         struct SignAndBitwidthPair
         {
             public SignAndBitwidthPair(bool isSigned, IntegerBitWidth bitWidth)
@@ -128,33 +165,45 @@ namespace HisRoyalRedness.com
 
             public bool IsSigned;
             public IntegerBitWidth BitWidth;
+            string DisplayString => $"{(IsSigned ? "I" : "U")}{(int)BitWidth}";
         }
 
+        [DebuggerDisplay("Min: {Min}, Max: {Max}, Mask: {Mask}")]
         struct MinAndMax
         {
-            public MinAndMax(BigInteger min, BigInteger max)
+            public MinAndMax(BigInteger min, BigInteger max, BigInteger mask)
             {
                 Min = min;
                 Max = max;
+                Mask = mask;
+            }
+
+            public MinAndMax(IntegerBitWidth bitWidth, bool isSigned)
+            {
+                Mask = BigInteger.Pow(2, (int)bitWidth) - 1;
+                if (isSigned)
+                {
+                    var num = BigInteger.Pow(2, (int)bitWidth - 1);
+                    Min = -num;
+                    Max = num - 1;
+                }
+                else
+                {
+                    Min = 0;
+                    Max = Mask;
+                }
             }
 
             public BigInteger Min;
             public BigInteger Max;
+            public BigInteger Mask;
         }
 
-        static readonly Dictionary<SignAndBitwidthPair, MinAndMax> _minAndMax = new Dictionary<SignAndBitwidthPair, MinAndMax>()
-        {
-            { new SignAndBitwidthPair(false, IntegerBitWidth._4), new MinAndMax(0, 15) },
-            { new SignAndBitwidthPair(false, IntegerBitWidth._8), new MinAndMax(0, 15) },
-            { new SignAndBitwidthPair(false, IntegerBitWidth._16), new MinAndMax(0, 15) },
-            { new SignAndBitwidthPair(false, IntegerBitWidth._32), new MinAndMax(0, 15) },
-            { new SignAndBitwidthPair(false, IntegerBitWidth._64), new MinAndMax(0, 15) },
-            { new SignAndBitwidthPair(false, IntegerBitWidth._128), new MinAndMax(0, 15) },
-
-        };
+        static readonly Dictionary<SignAndBitwidthPair, MinAndMax> _minAndMax = new Dictionary<SignAndBitwidthPair, MinAndMax>();
 
         public bool IsSigned { get; private set; }
         public IntegerBitWidth BitWidth { get; private set; }
+        public bool IsUnbound => BitWidth == IntegerBitWidth.Unbound;
 
         #region Equality
         public override bool Equals(IntegerToken other) => other == null ? false : (TypedValue == other.TypedValue);
@@ -164,12 +213,15 @@ namespace HisRoyalRedness.com
                 return true;
             if (a is null || b is null)
                 return false;
+            if (a.IsUnbound && b.IsUnbound)
+                return a.TypedValue == b.TypedValue;
             return a.TypedValue == b.TypedValue;
+
         }
         public static bool operator !=(IntegerToken a, IntegerToken b) => !(a == b);
         #endregion Equality
 
-        public override string ToString() => BitWidth == IntegerBitWidth.Unbound
+        public override string ToString() => IsUnbound
             ? $"{TypedValue}"
             : $"{TypedValue}{(IsSigned ? "I" : "U")}{BitWidth.GetEnumDescription()}";
     }
