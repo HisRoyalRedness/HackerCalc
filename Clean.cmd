6a5b9ef7fc62f7d6f16da42e59fe404b989399de
Keith Fletcher <keith@fletcher.org>
2019-05-28 12:06:59 +1200

    Oops

--- Clean.cmd
+++ Clean.cmd
@@ -1,5 +1,5 @@
 @echo off
 cls
 powershell -nologo -executionpolicy remotesigned -command "& { %~dpn0.ps1 }"
-"%~dp0HackerCalc.Parser\Coco\PreBuild.cmd" "%~dp0HackerCalc.Parser" "Debug"
+call "%~dp0HackerCalc.Parser\Coco\PreBuild.cmd" "%~dp0HackerCalc.Parser" "Debug"
 pause
